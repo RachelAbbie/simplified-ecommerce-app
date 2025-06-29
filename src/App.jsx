6fc685f6bfd34a17b56cde8ca1cd6938fe8b70fa
RachelAbbie <rachelabbief@gmail.com>
2025-06-29 13:18:27 +0800

    feat: add tailwindcss for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
-
 function App() {
 
   return (
-    <>
-     <h1>Hello World</h1>
-    </>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+          <h1 className="text-5xl font-extrabold text-blue-600">
+        Hello Tailwind
+      </h1>
+    </div>
+  );
 }
 
 export default App
